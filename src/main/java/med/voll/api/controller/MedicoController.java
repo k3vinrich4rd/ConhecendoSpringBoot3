083dfd31135f0c9044b939692f678dd6da1852be
k3vinrich4rd <kevinrichardve@gmail.com>
2023-07-24 08:28:34 -0300

    Inserindo paginacao de medicos.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,10 +6,11 @@ import med.voll.api.medico.dto.request.DadosCadastroMedicoRequestDto;
 import med.voll.api.medico.dto.response.DadosCadastroMedicoResponseDto;
 import med.voll.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "/medicos")
@@ -26,9 +27,9 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosCadastroMedicoResponseDto> buscarMedicos() {
-        return medicoRepository.findAll()
-                .stream()
-                .map(DadosCadastroMedicoResponseDto::new).toList();
+    //Pageable padrão, mas é possível sobrescrever fazendo a busca
+    public Page<DadosCadastroMedicoResponseDto> buscarMedicos(@PageableDefault(size = 10, sort = {"nome"}, page = 0) Pageable paginacao) {
+        return medicoRepository.findAll(paginacao)
+                .map(DadosCadastroMedicoResponseDto::new);
     }
 }
